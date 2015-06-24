f6ef156e65d1725b1a983d1d606fa61337536166
Mike <m@thecoded.org>
2015-06-24 11:55:29 +0900

    pinax and openshift and come on 3

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -7,23 +7,11 @@ BASE_DIR = PACKAGE_ROOT
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": "dev.db",
-    }
-}
-
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['OPENSHIFT_APP_NAME'],
-        'USER': os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
-        'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
-        'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],
-        'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
     }
 }
 
