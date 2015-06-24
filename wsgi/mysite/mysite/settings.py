fa7df3bffabdc5a82a9cbdde8530f3a13dc713bc
Mike <m@thecoded.org>
2015-06-24 14:22:36 +0900

    editing the settings

--- wsgi/mysite/mysite/settings.py
+++ wsgi/mysite/mysite/settings.py
@@ -3,7 +3,6 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
-#PACKAGE_ROOT = os.environ['OPENSHIFT_REPO_DIR']+"wsgi/mysite/mysite"
 BASE_DIR = PACKAGE_ROOT
 
 DEBUG = True
@@ -19,7 +18,6 @@ DATABASES = {
         'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
     }
 }
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -59,11 +57,11 @@ MEDIA_URL = "/site_media/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
-STATIC_ROOT = PACKAGE_ROOT
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
+
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
+STATIC_URL = "/site_media/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
