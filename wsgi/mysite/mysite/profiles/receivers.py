36f25f83505bfc8dade8f74bcb8f44e4df1e1ca2
Mike <m@thecoded.org>
2015-06-25 09:42:22 +0900

    post_save

--- wsgi/mysite/mysite/profiles/receivers.py
+++ wsgi/mysite/mysite/profiles/receivers.py
@@ -10,4 +10,6 @@ from .models import Profile
 @receiver(post_save, sender=User)
 def handle_user_save(sender, created, instance, **kwargs):
     if created:
-        Profile.objects.get_or_create(user=instance)
+        #Profile.objects.get_or_create(user=instance)
+        profile = Profile(user=User)
+        profile.save()
